0986a82f80588606492706230e64a9226d1a32a6
Sunbabaara <csunbabaara@gmail.com>
2023-06-18 14:11:39 +0530

    added navbar

---
+++ Nav bars/SideBar/day 2.py
@@ -0,0 +1,6 @@
+def mul(a,b):
+    return a*b
+a=int(input(print('enter first number: ')))
+b=int(input(print('enter second number: ')))
+v=mul(a,b)
+print('your answer is:',v)
---
+++ Week one/Forms1/justice.py
@@ -0,0 +1,10 @@
+p=[1,2,3,4,5,6,7,8,9,10]
+for l in p:
+        if l%2==0:
+            print(l)
+        else:
+            print('default')
+            O={'NAME':'JOHN','AGE':24}
+for j in O:
+     print(j)
+   
